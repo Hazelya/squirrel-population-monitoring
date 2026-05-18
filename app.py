6f9764c2cf35d9f25beed2a813da0d110007a9fd
Julia AZEAU <120457907+Hazelya@users.noreply.github.com>
2026-05-18 10:39:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,27 @@ from flask import Flask
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
+# Exemple
+# Connexion base de données 
+# requête simple
+# Route HTML
+# Affichage de la requête
+
 load_dotenv()
 
 app = Flask(__name__)
 
-supabase: Client = create_client(
+supabase: Client = create_client( # Connexion BDD
     os.environ.get("SUPABASE_URL"),
     os.environ.get("SUPABASE_KEY")
 )
 
-@app.route('/')
+@app.route('/') # Route à l'origine
 def index():
-    response = supabase.table('detections').select("*").execute()
-    todos = response.data
+    response = supabase.table('detections').select("*").execute() # Requete
+    todos = response.data # Résultat de la requete
 
+    # HTML
     html = '<h1>TEST NUMERO 1</h1><ul>'
     for todo in todos:
         html += f'<li>{todo["id_detection"]}</li>'
@@ -24,5 +31,5 @@ def index():
 
     return html
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Lancement du serveur (app.py) en local 
     app.run(debug=True)
